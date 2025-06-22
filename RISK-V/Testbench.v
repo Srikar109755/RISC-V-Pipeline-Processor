061674171db30ebde95f2ae1261098c75c9d5675
Sai Srikar <saisrikar109@gmail.com>
2025-06-22 22:21:10 +0530

    Update Testbench.v

--- RISK-V/Testbench.v
+++ RISK-V/Testbench.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.06.2025 18:29:13
-// Design Name: 
-// Module Name: Testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Testbench;
